5ba6bb463ebcf679f477ee525afff5feeaa24060
Graham Christensen <graham@grahamc.com>
2016-12-02 19:02:51 -0500

    Improve the ported.sh script to ... well, actually work.
    The version in this repo was not exactly what I was using. Sorry.

--- ported.sh
+++ ported.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env bash
+
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p git
 
 set -eu
+set -o pipefail
 
+TMPDIR="/tmp"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 REMOTE="origin"
@@ -18,15 +22,35 @@ cleanup() {
 }
 trap cleanup EXIT
 
+debug() {
+    set +u
+    if ! [ "x$DEBUG" = "x" ]; then
+        echo "$@" >&2
+    fi
+    set -u
+}
 
-git fetch "$REMOTE"
-git cherry "$REMOTE/$RELEASE_NAMED" "$REMOTE/$UPSTREAM" "$UPSTREAM_OLDEST" \
+#git fetch "$REMOTE"
+
+cherries() {
+    debug "cherries: Fetching"
+    git cherry "$REMOTE/$RELEASE_NAMED" "$REMOTE/$UPSTREAM" "$UPSTREAM_OLDEST"
+    debug "cherries: fetched"
+}
+
+logs() {
+    debug "logs: Fetching"
+    git log --pretty=format:%H "$TO_OLDEST"..."$REMOTE/$RELEASE_NAMED"
+    debug "logs: fetched"
+}
+
+cherries \
     | grep -v "+" \
     | cut -d' ' -f2 \
     | xargs git show \
     | git patch-id --stable > $TMPTO/upstream
 
-git log --pretty=format:%H "$TO_OLDEST"..."$REMOTE/$RELEASE_NAMED" \
+logs \
     | xargs git show \
     | git patch-id --stable > $TMPTO/against
 
@@ -54,17 +78,46 @@ LATEST_ROUNDUP_URL.
 EOF
 
 table() {
-  echo "$UPSTREAM|$TO_SHORT|Message|Notes"
-  echo "---|---|---|---"
   for id in `cat $TMPTO/upstream | cut -d' ' -f1`; do
-      master=$(git rev-parse --short $(cat $TMPTO/upstream | grep -G "^${id}" | cut -d' ' -f2))
-      release=$(git rev-parse --short $(cat $TMPTO/against | grep -G "^${id}" | cut -d' ' -f2))
-      message=$(git log --pretty=format:%s $master...$master~1)
+      debug "${id}: identify master hash"
+      master_hash=$(cat $TMPTO/upstream | grep -G "^${id}" | cut -d' ' -f2 | tail -n1)
+      debug "${id}: master -> $master_hash"
+
+      set +e
+      master_short=$(git rev-parse --short "${master_hash}")
+      if [ "x" = "x$master_short" ]; then
+          debug "${id}: BUG? Cannot find hash ${master_hash}"
+          exit 1
+      fi
+      set -e
+
+      debug "${id}: master -> $master_hash -> ${master_short}"
+
+      debug "${id}: identify release hash"
+      set +e
+      release_hash=$(cat $TMPTO/against | grep -G "^${id}" | cut -d' ' -f2)
+      if [ "x" = "x$release_hash" ]; then
+          debug "${id}: did not identify candidate in release, skipping."
+          continue
+      fi
+      set -e
+      debug "${id}: release -> $release_hash"
+      release_short=$(git rev-parse --short "${release_hash}")
+      debug "${id}: master -> $master_hash -> ${release_short}"
+
+      debug "${id}: fetch  message"
+      message=$(git log --pretty=format:%s $master_hash...$master_hash~1)
       if [ $(echo "$message" | wc -c) -gt 50 ]; then
          message=$(echo -n "$message" | head -c47; echo "...");
       fi
-      echo "$master|$release|$message|n/a"
+      echo "$master_short|$release_short|$message|n/a" >> $TMPTO/table
+      debug ""
   done
+
+  echo "$UPSTREAM|$TO_SHORT|Message|Notes"
+  echo "---|---|---|---"
+
+  cat "$TMPTO/table" | sort -t\| -k3
 }
 
 table | column -s"|" -t
